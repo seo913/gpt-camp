0bee152aa4294b29077e72f1fda47dcfe90df55f
seojaemin <seojaemin@seojaemin-ui-MacBookPro.local>
2023-05-09 10:17:31 +0900

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,149 @@
+import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faFire, faFishingRod, faHiking, faMountain } from '@fortawesome/free-solid-svg-icons'
+// import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <Head>
+        <title>Camp Out - Camping Adventures</title>
+        <meta name="description" content="Discover the great outdoors with Camp Out - the ultimate camping experience." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header className="bg-green-600 text-white">
+        <div className="container mx-auto py-4 px-6 flex items-center justify-between">
+          <div>
+            <h1 className="text-4xl font-bold">Camp Out</h1>
+            <p className="text-lg">Discover the great outdoors with us</p>
+          </div>
+          <Link href="/login">
+            <a className="px-4 py-2 bg-white text-green-600 rounded-lg font-medium hover:bg-green-700 hover:text-white transition duration-300">Login</a>
+          </Link>
         </div>
-      </div>
+      </header>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <main className="bg-gray-100">
+        <div className="container mx-auto py-12 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div className="flex items-center justify-center bg-white rounded-lg shadow-lg">
+              <Image src="/tent.png" width={400} height={400} alt="Tent" />
+            </div>
+            <div>
+              <h2 className="text-3xl font-bold mb-4">Welcome to Camp Out</h2>
+              <p className="text-lg mb-6">Camp Out is the ultimate camping experience, where you can explore the great outdoors and make memories that will last a lifetime. With a wide variety of activities to choose from, you're sure to find something you love.</p>
+              <Link href="/activities">
+                <a className="px-4 py-2 bg-green-600 text-white rounded-lg font-medium hover:bg-green-700 transition duration-300">Explore Activities</a>
+              </Link>
+            </div>
+          </div>
+        </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div className="bg-green-600 py-12 px-6 text-white">
+          <div className="container mx-auto">
+            <h2 className="text-3xl font-bold mb-4">Our Activities</h2>
+            <p className="text-lg mb-6">We offer a wide variety of activities to suit everyone's interests. Here are just a few of our most popular options:</p>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+              <div className="flex items-center justify-start">
+                {/* <FontAwesomeIcon icon={faHiking} className="text-5xl mr-4" /> */}
+                
+ </div>
+ <div className="flex items-center justify-start">
+   {/* <FontAwesomeIcon icon={faMountain} className="text-5xl mr-4" /> */}
+   <div>
+     <h3 className="text-2xl font-bold mb-2">Rock Climbing</h3>
+     <p>Experience the thrill of scaling the rock face with our expert guides.</p>
+   </div>
+ </div>
+ <div className="flex items-center justify-start">
+   {/* <FontAwesomeIcon icon={faFire} className="text-5xl mr-4" /> */}
+   <div>
+     <h3 className="text-2xl font-bold mb-2">Campfire</h3>
+     <p>Relax around the campfire and share stories with your fellow campers.</p>
+   </div>
+ </div>
+</div>
+</div>
+</div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+<div className="container mx-auto py-12 px-6">
+<div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+<div>
+ <h2 className="text-3xl font-bold mb-4">Our Mission</h2>
+ <p className="text-lg mb-6">Our mission at Camp Out is to provide a safe and enjoyable camping experience for all of our guests. We believe that spending time in nature is essential for physical and mental well-being, and we are committed to making the great outdoors accessible to everyone.</p>
+ <p className="text-lg mb-6">We strive to minimize our impact on the environment and promote sustainable practices in everything we do. By working together, we can preserve our natural resources for future generations to enjoy.</p>
+ <Link href="/about">
+   <a className="px-4 py-2 bg-green-600 text-white rounded-lg font-medium hover:bg-green-700 transition duration-300">Learn More About Us</a>
+ </Link>
+</div>
+<div className="flex items-center justify-center bg-white rounded-lg shadow-lg">
+ <Image src="/campfire.png" width={400} height={400} alt="Campfire" />
+</div>
+</div>
+</div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+<div className="bg-green-600 py-12 px-6 text-white">
+<div className="container mx-auto">
+<h2 className="text-3xl font-bold mb-4">Contact Us</h2>
+<p className="text-lg mb-6">If you have any questions or would like to book your camping adventure, please get in touch with us. Our friendly staff are always here to help.</p>
+<form>
+ <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+   <div>
+     <label htmlFor="name" className="block text-lg mb-2">Name</label>
+     <input type="text" id="name" name="name" className="border-gray-400 border-2 px-4 py-2 w-full rounded-lg" required />
+   </div>
+   <div>
+     <label htmlFor="email" className="block text-lg mb-2">Email</label>
+     <input type="email" id="email" name="email" className="border-gray-400 border-2 px-4 py-2 w-full rounded-lg" required />
+   </div>
+ </div>
+ <div className="mt-6">
+   <label htmlFor="message" className="block text-lg mb-2">Message</label>
+   <textarea     id="message" name="message" rows="6" className="border-gray-400 border-2 px-4 py-2 w-full rounded-lg" required></textarea>
+              </div>
+              <div className="mt-6">
+                <button type="submit" className="px-4 py-2 bg-white text-green-600 rounded-lg font-medium hover:bg-gray-100 transition duration-300">Send Message</button>
+              </div>
+            </form>
+          </div>
+        </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </main>
+
+      <footer className="bg-gray-200 py-8 px-6">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            <div>
+              <h3 className="text-lg font-bold mb-4">Contact Info</h3>
+              {/* <p className="mb-2"><FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2" /> 123 Main St, Anytown USA</p> */}
+              {/* <p className="mb-2"><FontAwesomeIcon icon={faPhone} className="mr-2" /> (555) 555-1234</p> */}
+              {/* <p><FontAwesomeIcon icon={faEnvelope} className="mr-2" /> info@campout.com</p> */}
+            </div>
+            <div>
+              <h3 className="text-lg font-bold mb-4">Links</h3>
+              <ul>
+                <li className="mb-2"><Link href="/"><a>Home</a></Link></li>
+                <li className="mb-2"><Link href="/about"><a>About Us</a></Link></li>
+                <li className="mb-2"><Link href="/activities"><a>Activities</a></Link></li>
+                <li className="mb-2"><Link href="/contact"><a>Contact Us</a></Link></li>
+              </ul>
+            </div>
+            <div>
+              <h3 className="text-lg font-bold mb-4">Follow Us</h3>
+              <div className="flex items-center">
+                {/* <FontAwesomeIcon icon={faFacebook} className="text-3xl mr-4" />
+                <FontAwesomeIcon icon={faTwitter} className="text-3xl mr-4" />
+                <FontAwesomeIcon icon={faInstagram} className="text-3xl" /> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+    </div>
+  );
 }
+   
